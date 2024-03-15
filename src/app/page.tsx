8db69a8f7284e41d3c7079496375656520d95b80
hongsamhc2 <hongsamgcp@gmail.com>
2024-03-15 17:46:49 +0900

    [FEATURE] : 프로젝트 생성

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import Head from "next/head";
+import {Metadata} from "next";
+import styles from "./test.module.scss"
+
+export const metadata: Metadata = {
+    title: "HIIO420",
+    description: "Hiio420Porfolio"
+}
+export default function Home() {
+    return (
+        <>
+            <main className={styles.home}>
+                HIIO420
+            </main>
+        </>
+    );
+}
